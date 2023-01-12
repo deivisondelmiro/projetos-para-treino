ed193b67a313882462e4982ddad96705d1f342a2
Deivison Delmiro <deivisondelmirodasilva@gmail.com>
2023-01-12 01:08:05 -0300

    projeto article

--- frontendmentor/iniciante/09-article-preview-component-master/js/main.js
+++ frontendmentor/iniciante/09-article-preview-component-master/js/main.js
@@ -1,8 +1,9 @@
-const iconShare = document.querySelector('#icon-share')
+const iconShare = document.querySelector('.profile-share')
 const containerMedia = document.querySelector('.container-media')
 
 function showIconMedia() {
-  containerMedia.classList.toggle('active')
+  containerMedia.classList.toggle('media-active')
+  iconShare.classList.toggle('share-active')
 }
 
 iconShare.addEventListener('click', showIconMedia)
