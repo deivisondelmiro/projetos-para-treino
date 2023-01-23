a28ff7c4a827a0776d4fd02ab3f868267afba973
Deivison Delmiro <deivisondelmirodasilva@gmail.com>
2023-01-23 18:44:31 -0300

    reorganização de repositórios

--- frontendmentor/iniciante/09-article-preview-component-master/js/main.js
+++
@@ -1,9 +0,0 @@
-const iconShare = document.querySelector('.profile-share')
-const containerMedia = document.querySelector('.container-media')
-
-function showIconMedia() {
-  containerMedia.classList.toggle('media-active')
-  iconShare.classList.toggle('share-active')
-}
-
-iconShare.addEventListener('click', showIconMedia)
